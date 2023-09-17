5c047aec811cfc697eabcc5ed761568d57cf74c0
duvan321 <diegoduvan321@gmail.com>
2023-09-17 14:34:08 -0500

    terminado mi app

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import SearchBar from "./SearchBar";
+import MoviePreview from "./MoviePreview";
+
+function App() {
+  const API_URL = "https://api.themoviedb.org/3";
+  const API_KEY = "4f5f43495afcc67e9553f6c684a82f84";
+
+  // variables de estado
+  const [movies, setMovies] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
+  const [trailer, setTrailer] = useState(null);
+  const [movie, setMovie] = useState({ title: "Loading Movies" });
+  const [playing, setPlaying] = useState(false);
+
+  // funcion para realizar la peticion get a la api
+  const fetchMovies = async (searchKey) => {
+    const type = searchKey ? "search" : "discover";
+    const {
+      data: { results },
+    } = await axios.get(`${API_URL}/${type}/movie`, {
+      params: {
+        api_key: API_KEY,
+        query: searchKey,
+      },
+    });
+
+    setMovies(results);
+    setMovie(results[0]);
+
+    if (results.length) {
+      await fetchMovie(results[0].id);
+    }
+  };
+
+  // funcion para la peticion de un solo objeto y mostrar en reproductor de videos
+  const fetchMovie = async (id) => {
+    const { data } = await axios.get(`${API_URL}/movie/${id}`, {
+      params: {
+        api_key: API_KEY,
+        append_to_response: "videos",
+      },
+    });
+
+    if (data.videos && data.videos.results) {
+      const trailer = data.videos.results.find(
+        (vid) => vid.name === "Official Trailer"
+      );
+      setTrailer(trailer ? trailer : data.videos.results[0]);
+    }
+
+    setMovie(data);
+  };
+
+  const selectMovie = async (movie) => {
+    fetchMovie(movie.id);
+
+    setMovie(movie);
+    window.scrollTo(0, 0);
+  };
+
+  // funcion para buscar peliculas
+  const searchMovies = (e) => {
+    e.preventDefault();
+    fetchMovies(searchKey);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <h2 className="text-center mt-5 mb-5 text-light border border-success bg bg-success p-3">
+        Los mejores trailer para escojer tus mejores peliculas del año
+      </h2>
+
+      <SearchBar
+        searchKey={searchKey}
+        setSearchKey={setSearchKey}
+        searchMovies={searchMovies}
+      />
+
+      <MoviePreview
+        movie={movie}
+        trailer={trailer}
+        playing={playing}
+        setPlaying={setPlaying}
+        selectMovie={selectMovie}
+      />
+
+      <div className="container mt-3 b">
+        <div className="row">
+          {movies.map((movie) => (
+            <div
+              key={movie.id}
+              className="col-md-4 mb-3"
+              onClick={() => selectMovie(movie)}
+            >
+              <img
+                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                alt={movie.title}
+                height={600}
+                width="100%"
+              />
+              <h4 className="text-center text-success">{movie.title}</h4>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
